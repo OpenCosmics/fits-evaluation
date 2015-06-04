8c41e7959262a695b395018e3ebc9457183cc4f4
Anirudha Bose <ani07nov@gmail.com>
2015-06-04 22:46:33 +0530

    Parse event data as CSV, and build a Python dict
     - Remove use of Numpy's genfromtxt()
     - Support for JSON dump
     - Added documentation of the data set

--- station_data.py
+++ station_data.py
@@ -7,6 +7,7 @@ import json
 from pprint import pprint
 from StringIO import StringIO
 from numpy import genfromtxt
+import csv
 
 def extract_station_data(code):
     url = "http://data.hisparc.nl/api/station/%d/"%(code)
@@ -25,37 +26,83 @@ def get_stations():
         data.append(station['number'])
     return data
 
+
 def get_events(station, start, end):
+    """
+    Event Summary Data
+    ------------------
+    date:                time of event [GPS calendar date]
+    time:                time of event [GPS time of day]
+    timestamp:           time of event [UNIX timestamp]
+    nanoseconds:         time of event [number of nanoseconds after timestamp]
+    pulseheights (4x):   maximum signal pulseheight [ADC]
+    integral (4x):       integral of the signal [ADC.ns]
+    number_of_mips (4x): estimate for the number of particles in the detector
+    arrival_times (4x):  relative time of arrival of the first particle in the
+                         detector [ns]
+    trigger_time:        relative time of the trigger timestamp [ns]
+
+    Values of -1 for detectors 3 and 4 indicate that the station only has
+    two detectors.  Values of -999 indicate a problem in the analysis of
+    that particular event.  This may be the result from noise in the signal.
+    """
+
+    event = {}
+    event_list = []
     url = 'http://data.hisparc.nl/data/'+str(station)+'/events'
     query = urlencode({'download': False, 'start': start,'end': end})
-    full_url = url + '?' + query
-    data = urlopen(full_url).read()
-    format = [('date', 'datetime64[D]'), ('time', '|S8'),
-              ('timestamp', 'uint32'), ('nanoseconds', 'uint32'),
-              ('pulseheights', '4int16'), ('integrals', '4int32'),
-              ('n1', 'float32'), ('n2', 'float32'),
-              ('n3', 'float32'), ('n4', 'float32'),
-              ('t1', 'float32'), ('t2', 'float32'),
-              ('t3', 'float32'), ('t4', 'float32'),
-              ('t_trigger', 'float32')]
-    a = genfromtxt(StringIO(data), delimiter="\t", dtype=format)
-    return a
 
-def eventToJSON(event, station_data):
-    tempDict = [{'date': event[0], 'time': event[1], 'timestamp': event[2],
-                'nanoseconds': event[3], 'altitude': station_data['altitude'], 
-                'latitude': station_data['latitude'], 'longitude': station_data['longitude']}]
-    return tempDict
+    # Read the string as a file object
+    data = StringIO(urlopen(url + '?' + query).read())
+
+    csv_obj = csv.reader(data, delimiter = '\t')
+    for row in csv_obj:
+        # Ignore rows starting with a # (comments)
+        if row[0].startswith('#'):
+            continue
+
+        # Do NOT convert date and time to Python formats as they are not JSON
+        # serializable
+        event['date'] = row[0]
+        event['time'] = row[1]
+
+        event['timestamp'] = int(row[2])
+        event['nanoseconds'] = int(row[3])
+
+        event['pulse_heights'] = [int(row[4]), int(row[5]),
+                                  int(row[6]), int(row[7])]
+
+        event['integral'] = [int(row[8]), int(row[9]),
+                             int(row[10]), int(row[11])]
+
+        event['number_of_mips'] = [float(row[12]), float(row[13]),
+                                   float(row[14]), float(row[15])]
+
+        event['arrival_times'] = [float(row[16]), float(row[17]),
+                                  float(row[18]), float(row[19])]
+
+        event['trigger_time'] = float(row[20])
+
+        event_list.append(event)
+
+    return event_list
+
 
 def main():
-	station_ids = [3] #get_stations()
+	station_ids = [3]
 	for code in station_ids:
-		outputStation = extract_station_data(code)
-		if outputStation is not None:
-			pprint(outputStation)
-		eventOutput = get_events(code,datetime(2013, 7, 2, 11, 0),datetime(2013, 7, 2, 11, 05))
-		for event in eventOutput:
-			print eventToJSON(event, outputStation)
+		output_station = extract_station_data(code)
+		if output_station is not None:
+			pprint(output_station)
+		event_output = get_events(code, datetime(2013, 7, 2, 11, 0),
+                                  datetime(2013, 7, 2, 11, 05))
+
+        # Event output as Python dictionary
+        pprint(event_output)
+
+        # To generate a JSON dump, uncomment the code below
+        # print(json.dumps(event_output, sort_keys=True, indent=4))
+
 
 if __name__ == '__main__':
     main()
