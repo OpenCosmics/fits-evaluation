a79d7cc61985c4b6a4ae1696dcd005793e91001d
Frank Berghaus <frank.berghaus@cern.ch>
2015-06-04 14:56:32 +0200

    Retrieve events from a station
    For nos just use station 3 with some random dates that have events. Get
    the station information get events in that time. Just returns a string
    for each event, need to format properly to make this easy for JSON to
    understand.

--- station_data.py
+++ station_data.py
@@ -1,6 +1,12 @@
+from urllib2 import urlopen
+from datetime import date, datetime
+from urllib import urlencode
+#from pylab import plot, show
 import urllib2
 import json
 from pprint import pprint
+from StringIO import StringIO
+from numpy import genfromtxt
 
 def extract_station_data(code):
     url = "http://data.hisparc.nl/api/station/%d/"%(code)
@@ -19,12 +25,30 @@ def get_stations():
         data.append(station['number'])
     return data
 
+def get_events(station, start, end):
+    url = 'http://data.hisparc.nl/data/'+str(station)+'/events'
+    query = urlencode({'download': False, 'start': start,'end': end})
+    full_url = url + '?' + query
+    data = urlopen(full_url).read()
+    format = [('date', 'datetime64[D]'), ('time', '|S8'),
+              ('timestamp', 'uint32'), ('nanoseconds', 'uint32'),
+              ('pulseheights', '4int16'), ('integrals', '4int32'),
+              ('n1', 'float32'), ('n2', 'float32'),
+              ('n3', 'float32'), ('n4', 'float32'),
+              ('t1', 'float32'), ('t2', 'float32'),
+              ('t3', 'float32'), ('t4', 'float32'),
+              ('t_trigger', 'float32')]
+    a = genfromtxt(StringIO(data), delimiter="\t", dtype=format)
+    return a
+
 def main():
-    station_ids = get_stations()
+    station_ids = [3] #get_stations()
     for code in station_ids:
         out = extract_station_data(code)
         if out is not None:
             pprint(out)
+        out = get_events(code,datetime(2013, 7, 2, 11, 0),datetime(2013, 7, 2, 12, 0))
+        print(out)
 
 if __name__ == '__main__':
     main()
