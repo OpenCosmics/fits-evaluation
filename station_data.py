f47cfe763821e4794b7d109f4e04281190465e3d
Anirudha Bose <ani07nov@gmail.com>
2015-06-05 19:12:36 +0530

    Implement Frank's mind map
    https://www.mindmup.com/#m:a150006750edb50132dfcf1222860cd564

--- station_data.py
+++ station_data.py
@@ -8,7 +8,7 @@ from StringIO import StringIO
 from numpy import genfromtxt
 import csv
 
-def extract_station_data(code):
+def get_station_data(code):
     url = "http://data.hisparc.nl/api/station/%d/"%(code)
     try:
         response = json.loads(urllib2.urlopen(url).read())
@@ -54,6 +54,9 @@ def get_events(station, start, end):
     # Read the string as a file object
     data = StringIO(urllib2.urlopen(url + '?' + query).read())
 
+    # Read the station data
+    station_data = get_station_data(station)
+
     csv_obj = csv.reader(data, delimiter = '\t')
     for row in csv_obj:
         # Ignore rows starting with a # (comments)
@@ -62,45 +65,52 @@ def get_events(station, start, end):
 
         # Do NOT convert date and time to Python formats as they are not JSON
         # serializable
-        event['date'] = row[0]
-        event['time'] = row[1]
 
-        event['timestamp'] = int(row[2])
-        event['nanoseconds'] = int(row[3])
+        event['location'] = {
+                             'latitude'  : station_data['latitude'],
+                             'longitude' : station_data['longitude'],
+                             'altitude'  : station_data['altitude']
+                            }
 
-        event['pulse_heights'] = [int(row[4]), int(row[5]),
-                                  int(row[6]), int(row[7])]
+        event['time'] = row[0] + ' ' + row[1] + '%9d'%(int(row[2]))
 
-        event['integral'] = [int(row[8]), int(row[9]),
-                             int(row[10]), int(row[11])]
+        event['window'] = None
 
-        event['number_of_mips'] = [float(row[12]), float(row[13]),
-                                   float(row[14]), float(row[15])]
+        event['livetime'] = None
 
-        event['arrival_times'] = [float(row[16]), float(row[17]),
-                                  float(row[18]), float(row[19])]
+        event['UID'] = station_data['number']
 
-        event['trigger_time'] = float(row[20])
+        event['status'] = 'active' if bool(station_data['active']) else 'inactive'
+
+        event['measurement'] = {
+                                'pulseheights'  : [int(row[4]), int(row[5]),
+                                                   int(row[6]), int(row[7])],
+
+                                'integral'      : [int(row[8]), int(row[9]),
+                                                   int(row[10]), int(row[11])],
+
+                                'number_of_mips': [float(row[12]), float(row[13]),
+                                                   float(row[14]), float(row[15])],
+
+                                'arrival_times' : [float(row[16]), float(row[17]),
+                                                   float(row[18]), float(row[19])],
+                                'trigger_time'  : float(row[20])
+                               }
 
         event_list.append(event)
 
     return event_list
 
-
 def main():
-	station_ids = [3]
-	for code in station_ids:
-		output_station = extract_station_data(code)
-		if output_station is not None:
-			pprint(output_station)
-		event_output = get_events(code, datetime(2013, 7, 2, 11, 0),
-                                  datetime(2013, 7, 2, 11, 05))
+    station_id = 3
+    events = get_events(station_id, datetime(2013, 7, 2, 11, 0),
+                                    datetime(2013, 7, 2, 11, 05))
 
-        # Event output as Python dictionary
-        pprint(event_output)
+    # Event output as Python dictionary
+    # pprint(events)
 
-        # To generate a JSON dump, uncomment the code below
-        # print(json.dumps(event_output, sort_keys=True, indent=4))
+    # Generate a JSON dump
+    print(json.dumps(events, sort_keys=True, indent=4))
 
 
 if __name__ == '__main__':
